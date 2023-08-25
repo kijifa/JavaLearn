d0fedae0c4a952a4270afd0f9c8291f9da31fc21
Kijifa <jiri.kifa@gmail.com>
2023-08-25 22:39:11 +0200

    First two tasks in course

--- Main.java
+++ Main.java
@@ -1,15 +1,13 @@
+import static challenges.MyInicials.writeMyInicials;
+import static workbooks.FirstProgram.firstProgram;
+
 public class Main {
     public static void main(String[] args) {
-        int answer = 421;
-        String text = "What is an answer on basic question?";
-
-        if (answer == 42) {
-            System.out.println(text);
-            System.out.println(answer);
-        }
-        else {
-            System.out.println("Nope");
-        }
-
+        System.out.println("Based on Udemy course");
+        System.out.println("Let the game begin");
+        System.out.println("Workbook 1.1");
+            firstProgram();
+        System.out.print("Challenge 1 - write an inicials");
+            writeMyInicials();
     }
 }
--- out/production/JavaLearn/README.md
+++ out/production/JavaLearn/README.md
@@ -1,2 +1,3 @@
 # JavaLearn
 Project for my basic steps in Java Programming :)
+Based on Udemy https://www.udemy.com/course/the-complete-java-development-bootcamp/
