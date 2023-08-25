e5d05793387ce8f3a7fa2bd07684e1faa43a68d9
Kijifa <jiri.kifa@gmail.com>
2023-08-25 19:22:27 +0200

    Variables learning

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int answer = 42;
+        String text = "What is an answer on basic question?";
+
+        System.out.println(text);
+        System.out.println(answer);
     }
 }
