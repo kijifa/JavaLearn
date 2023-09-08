e71b14298bd39193c6a479211c7c4b000f6c5a87
Kijifa <jiri.kifa@gmail.com>
2023-09-08 20:48:56 +0200

    Add Guess Number

--- workbooks/GuessNumber.java
+++ workbooks/GuessNumber.java
@@ -5,19 +5,16 @@ import java.util.Scanner;
 
 public class GuessNumber {
     int user_number = 0;
+    boolean done = false;
     Random rand = new Random();
     final int guess_number = rand.nextInt(10);
 
     public GuessNumber(){
         getNumber();
+        while (!done) {
+            compare();
+        }
 
-        if (user_number == guess_number) {
-            System.out.println("You got it! Both numbers are " + guess_number);
-        }
-        else {
-            System.out.println("You have missed! Your number is " + user_number
-                    + " but you should choose " + guess_number);
-        }
     }
 
     public void getNumber(){
@@ -30,6 +27,18 @@ public class GuessNumber {
         }
         user_number = value.nextInt();
         System.out.println("You have " + user_number);
+    }
+
+    public void compare(){
+        if (user_number == guess_number) {
+            System.out.println("You got it! Both numbers are " + guess_number);
+            done = true;
+        }
+        else {
+            System.out.println("You have missed! Your number is " + user_number
+                    + " but you should choose " + guess_number);
+
+        }
     }
 
 }
